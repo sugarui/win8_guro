2585ee32d91228878a862c9929a36406e6ca05bd
sugarui <sugarui@naver.com>
2012-11-19 15:20:14 +0900

    3단계
    [01]setInterval 의 var화  /  setIntartav 인자값0 시 clearInterval로 해당var 클리어,
    팝업  /  getElementById 라이브러리화

--- game/timer.js
+++ game/timer.js
@@ -1,4 +1,5 @@
-/*(
+/*아왜내가쓴거는안돼냐고
+(
 	function(	
 		var timer = 60 ;
 
@@ -10,15 +11,36 @@
 			1000
 		);
 	)
-)(); 아왜내가쓴거는안돼냐고*/	
+)(); 
+*/	
+
+/* 
+라이브러리 1. 못씀.  var로 선언하면 파일변수화되어버리는 이슈및 호이스팅 이슈가 있어서 라이브러리 2를 사용.
+var $ = function(id){
+	return document.getElementbyId("timer");
+}
+*/
+
+//라이브러리 2. 라이브러리의 기본개념.
+function $(id){
+	return document.getElementById(id);
+}
 
 (function (){
-	var timer = 60;
+	var timer = 5;
 	
-	setInterval(function(){
-		console.log(timer);
+	var nT = setInterval(function(){
+		// console.log(timer);
+		// var el = document.getElementById("timer");
+		var el = $("timer");
+		el.innerHTML = timer;
 		
 		timer--;
+		
+		if (timer<0){
+			clearInterval(nT);
+			alert ("Timeover");
+		}
+		
 	}, 1000);
-
 })();
