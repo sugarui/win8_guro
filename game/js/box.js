b08adea27dc78d1387d9cca482d6b87f0ccd201a
sugarui <sugarui@naver.com>
2012-11-19 17:52:11 +0900

    5단계
    영역내랜덤지속생성까지. 내코드로하니까 다시안됨. 일단귀가.

--- game/js/box.js
+++ game/js/box.js
@@ -1,5 +1,41 @@
-?(function(){
+var createbox = function (){
+(function(){
 
+	var elCont = $("#content") [0]; // 셀렉터가좀달라
+	var elBox = $('<div class="balloon"></div>');
+	var aColor =["red","blue","green","yellow","pink","purple","white"];
 
+	elBox.css({
+		"left" : Math.floor(Math.random() * 100),
+		"top" : Math.floor(Math.random() * 100),
+		"background-color" : aColor[Math.floor(Math.random() *100) %7]
+	}).fadeIn();
+	elCont.appendChild(elBox[0]);
+}	
+}
+	
+	setInterval(createBox, 500);  // 타이머 멈추는게 과제
+	
 
 })()
+
+/*
+(function(){
+	var createBox = function(){
+		var elCont = $("#content")[0];
+		var elBox = $('<div class="balloon"></div>');
+		var aColor = ["#342353","red","blue","#098343","#afed00","#2343de","#23ffe3"];
+
+
+		elBox.css({
+			"left": Math.floor(Math.random() * 10000) % 750,
+			"top" : Math.floor(Math.random() * 10000) % 350,
+			"background-color": aColor[Math.floor(Math.random() * 100) % 7]
+		}).fadeIn();
+		elCont.appendChild(elBox[0]);
+	}
+	
+	setInterval(createBox, 500);  // 타이머 멈추는게 과제
+
+})()
+*/
--- game/js/timer.js
+++ game/js/timer.js
@@ -21,20 +21,21 @@ var $ = function(id){
 }
 */
 
-//라이브러리 2. 라이브러리의 기본개념.
+/* 
+라이브러리 2. 라이브러리의 기본개념.. 근데 이제 정식제이쿼리랑 겹치니까 주석처리합니다 : 근데그담부터출력이안됨ㅠㅠ
 function $(id){
 	return document.getElementById(id);
 }
+*/
 
 (function (){
-	var timer = 5;
+	var timer = 100;
 	
 	var nT = setInterval(function(){
 		// console.log(timer);
 		// var el = document.getElementById("timer");
-		var el = $("timer");
-		el.innerHTML = timer;
-		
+		var el = $("#timer")[0]; //흑흑이인덱스0이뭘까
+		el.innerHTML = "<div>" + timer + "</div>";
 		timer--;
 		
 		if (timer<0){
@@ -44,3 +45,22 @@ function $(id){
 		
 	}, 1000);
 })();
+
+/*
+(function (){
+	var timer = 60;
+
+	var nT = setInterval(function(){
+		//console.log(timer);
+		//var el = document.getElementById("timer");
+		var el = $("#timer")[0];
+		el.innerHTML = "<div>" + timer + "</div>";
+		timer--;
+
+		if( timer < 0 ){
+			clearInterval(nT);
+			alert("Time Over");
+		}
+	}, 1000);
+})();
+*/
