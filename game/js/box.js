bac14c2f9fbc84072e5cf327a46f05f595a02cd9
sugarui <sugarui@naver.com>
2012-11-20 09:22:48 +0900

    단계 5.1
    작동하게 일단 복구

--- game/js/box.js
+++ game/js/box.js
@@ -1,22 +1,22 @@
-var createbox = function (){
+
 (function(){
-
-	var elCont = $("#content") [0]; // 셀렉터가좀달라
-	var elBox = $('<div class="balloon"></div>');
-	var aColor =["red","blue","green","yellow","pink","purple","white"];
-
-	elBox.css({
-		"left" : Math.floor(Math.random() * 100),
-		"top" : Math.floor(Math.random() * 100),
-		"background-color" : aColor[Math.floor(Math.random() *100) %7]
-	}).fadeIn();
-	elCont.appendChild(elBox[0]);
-}	
-}
+	var createBox = function (){
+		var elCont = $("#content") [0]; // 셀렉터가좀달라
+		var elBox = $('<div class="balloon"></div>');
+		var aColor =["red","orange","yellow","pink","green","blue","purple"];
+		
+		elBox.css(
+			{
+			"left" : Math.floor(Math.random() * 10000) % 750,
+			"top" : Math.floor(Math.random() * 10000) % 350,
+			"background-color" : aColor[Math.floor(Math.random() *100) %7]
+			}
+		).fadeIn();
+		
+		elCont.appendChild(elBox[0]);
+	}
 	
 	setInterval(createBox, 500);  // 타이머 멈추는게 과제
-	
-
 })()
 
 /*
